3d60c946053bf5eeddd87b55120b9e497a08720c
qjunx <52962309+qjunx@users.noreply.github.com>
2019-11-13 11:08:24 +0800

    项目初始化

--- src/router/index.js
+++ src/router/index.js
@@ -4,27 +4,27 @@ import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
 
-// import Home from '../pages/Home.vue';
-// import Reg from '../pages/Reg.vue';
-// import Login from '../pages/Login.vue';
-// import Mine from '../pages/Mine.vue';
-// import Cart from '../pages/Cart.vue';
-// import Goods from '../pages/Goods.vue';
-// import Elder from '../pages/Elder.vue';
-// import Friend from '../pages/Friend.vue';
-// import Lover from '../pages/Lover.vue';
-// import NotFound from '../pages/NotFound.vue';
+import Home from '../pages/Home.vue';
+import Reg from '../pages/Reg.vue';
+import Login from '../pages/Login.vue';
+import Mine from '../pages/Mine.vue';
+import Cart from '../pages/Cart.vue';
+import Goods from '../pages/Goods.vue';
+import Elder from '../pages/Elder.vue';
+import Friend from '../pages/Friend.vue';
+import Lover from '../pages/Lover.vue';
+import NotFound from '../pages/NotFound.vue';
 //懒加载
-const Home = () => import('./Home.vue');
-const Reg = () => import('./Reg.vue');
-const Login = () => import('./Login.vue');
-const Mine = () => import('./Mine.vue');
-const Cart = () => import('./Cart.vue');
-const Goods = () => import('./Goods.vue');
-const Elder = () => import('./Elder.vue');
-const Friend = () => import('./Friend.vue');
-const Lover = () => import('./Lover.vue');
-const NotFound = () => import('./NotFound.vue');
+// const Home = () => import('./Home.vue');
+// const Reg = () => import('./Reg.vue');
+// const Login = () => import('./Login.vue');
+// const Mine = () => import('./Mine.vue');
+// const Cart = () => import('./Cart.vue');
+// const Goods = () => import('./Goods.vue');
+// const Elder = () => import('./Elder.vue');
+// const Friend = () => import('./Friend.vue');
+// const Lover = () => import('./Lover.vue');
+// const NotFound = () => import('./NotFound.vue');
 const router = new VueRouter({
     routes: [{
             name: 'home',
