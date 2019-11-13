7ff905d42e1bc5fd3508188168504c0b252126b2
qjunx <52962309+qjunx@users.noreply.github.com>
2019-11-13 14:24:23 +0800

    修改

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,5 +1,38 @@
 <template>
-  <div>主页</div>
+  <div>
+    <el-container>
+      <el-header>
+        <el-col :span="1">
+          <el-menu
+            default-active="2"
+            class="el-menu-vertical-demo"
+            @open="handleOpen"
+            @close="handleClose"
+          >
+            <el-submenu index="1">
+              <template slot="title">
+                <i class="el-icon-menu"></i>
+                <span></span>
+              </template>
+              <el-menu-item-group>
+                <template slot="title">分组一</template>
+                <el-menu-item index="1-1">选项1</el-menu-item>
+                <el-menu-item index="1-2">选项2</el-menu-item>
+              </el-menu-item-group>
+              <el-menu-item-group title="分组2">
+                <el-menu-item index="1-3">选项3</el-menu-item>
+              </el-menu-item-group>
+              <el-submenu index="1-4">
+                <template slot="title">选项4</template>
+                <el-menu-item index="1-4-1">选项1</el-menu-item>
+              </el-submenu>
+            </el-submenu>
+          </el-menu>
+        </el-col>
+      </el-header>
+      <el-main>Main</el-main>
+    </el-container>
+  </div>
 </template>
 <script>
 export default {};
