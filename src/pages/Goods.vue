11bd6d7fbc1268f58664de394d75a8fb32d412e6
qjunx <52962309+qjunx@users.noreply.github.com>
2019-11-17 20:46:03 +0800

    11

--- src/pages/Goods.vue
+++ src/pages/Goods.vue
@@ -107,11 +107,11 @@
         </div>
       </div>
       <div class="cart">
-        <el-button type="danger" round>加入购物车咯</el-button>
+        <el-button type="danger" round @click="buy()">加入购物车咯</el-button>
       </div>
     </el-drawer>
     <div class="addCart">
-      <el-button type="danger" round>加入购物车♂</el-button>
+      <el-button type="danger" round @click="drawer = true">加入购物车♂</el-button>
     </div>
   </div>
   <div
@@ -139,6 +139,7 @@ export default {
       someList1: [],
       message: "",
       messageimg: [],
+      goodsvue: "",
       options1: {
         currentPage: 0,
         thresholdDistance: 100,
@@ -198,7 +199,7 @@ export default {
           style: {
             background:
               'url("//asset.ibanquan.com/image/5b5889e820663d15db000b71/s.jpeg?v=1532529128")left/100% 100%',
-            width: "33%"
+            width: "33.3%"
           }
         },
         {
@@ -226,7 +227,12 @@ export default {
       this.$router.push(path);
     },
     handleChange(value) {
-      console.log(value);
+      this.goodsvue = value;
+    },
+    buy() {
+      let useid = this.$store.state.common.user._id;
+      let gid = this.message.gid;
+      let num = this.goodsvue;
     }
   },
   async beforeCreate() {
@@ -236,8 +242,6 @@ export default {
       id: this.$route.params.id
     });
     this.message = data[0];
-    console.log(this.message);
-
     //轮播图
     // data[0].imgs
     //   .slice(1, -1)
--- src/store/cart.js
+++ src/store/cart.js
@@ -4,12 +4,10 @@ export default {
   // namespaced: true,
 
   state: {
-    goodslist: [
-      {
+    goodslist: [{
         id: "1",
         name: "huawei mate30 pro",
-        imgurl:
-          "https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=3089410232,3830777459&fm=11&gp=0.jpg",
+        imgurl: "https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=3089410232,3830777459&fm=11&gp=0.jpg",
         price: 5998,
         qty: 5,
         onecheck: false
@@ -17,8 +15,7 @@ export default {
       {
         id: "2",
         name: "xiaomi9",
-        imgurl:
-          "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1571131475&di=2df2d3a54a89db9e09952799acb25261&imgtype=jpg&er=1&src=http%3A%2F%2Fi0.hdslb.com%2Fbfs%2Farticle%2F8488db95efa140b9c50cb4615e2ca337a6981aa7.jpg",
+        imgurl: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1571131475&di=2df2d3a54a89db9e09952799acb25261&imgtype=jpg&er=1&src=http%3A%2F%2Fi0.hdslb.com%2Fbfs%2Farticle%2F8488db95efa140b9c50cb4615e2ca337a6981aa7.jpg",
         price: 2999,
         qty: 2,
         onecheck: false
@@ -26,8 +23,7 @@ export default {
       {
         id: "3",
         name: "onePlus9 pro",
-        imgurl:
-          "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1570536784660&di=d4471f6edf73cace7d98fb05869a9277&imgtype=0&src=http%3A%2F%2Fimg13.360buyimg.com%2Fn1%2Fs450x450_jfs%2Ft28117%2F273%2F1288839750%2F66834%2F8ef15c40%2F5cdd22b8Nbc711aba.jpg",
+        imgurl: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1570536784660&di=d4471f6edf73cace7d98fb05869a9277&imgtype=0&src=http%3A%2F%2Fimg13.360buyimg.com%2Fn1%2Fs450x450_jfs%2Ft28117%2F273%2F1288839750%2F66834%2F8ef15c40%2F5cdd22b8Nbc711aba.jpg",
         price: 3999,
         qty: 1,
         onecheck: false
@@ -37,7 +33,10 @@ export default {
 
   getters: {
     totalPrice(state) {
-          return state.goodslist.reduce((prev, item) =>  prev + item.price * item.qty, 0 );   
+      return state.goodslist.reduce((prev, item) => {
+        prev + item.price * item.qty
+      }, 0);
+
     }
   },
   mutations: {
